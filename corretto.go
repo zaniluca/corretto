68499e005d9399a0c6efc4ba3f7e18f452238bd3
Luca Zani <lucazani2002@gmail.com>
2024-04-12 22:52:47 +0200

    feat: Add optional function to handle variadic parameters

--- corretto.go
+++ corretto.go
@@ -2,7 +2,9 @@ package corretto
 
 import (
 	"log"
+	"path/filepath"
 	"reflect"
+	"runtime"
 )
 
 type Schema map[string]*Validator
@@ -21,6 +23,26 @@ type Validator struct {
 	validations []func() error
 }
 
+// Utility to return the first parameter of a variadic function and log a warning if more than one parameter is passed
+func optional[T any](params []T) T {
+	if len(params) == 1 {
+		return params[0]
+	} else if len(params) > 1 {
+		// Get the caller function name
+		pc, _, _, _ := runtime.Caller(1)
+		caller := runtime.FuncForPC(pc)
+		// Get the file and line number that the caller function was called
+		// This is done to show the correct file and line number in the log message
+		_, file, line, _ := runtime.Caller(2)
+		filename := filepath.Base(file)
+
+		log.Printf("WARN corretto: %s (%s:%d): calling with more than one parameter, only the first one will be used", caller.Name(), filename, line)
+	}
+
+	// Returns the zero value of the type
+	return *new(T)
+}
+
 // Field creates a new validator for a field.
 // Pass the field name to be displayed in the error message or leave it empty to use the struct field name.
 // Although you can pass fieldName as a variadic parameter this is done only to make it optional.
@@ -28,10 +50,8 @@ type Validator struct {
 //
 // Example: Field("Name")
 func Field(fieldName ...string) *Validator {
-	name := ""
-	if len(fieldName) > 0 {
-		name = fieldName[0]
-	}
+	name := optional(fieldName)
+
 	return &Validator{fieldName: name}
 }
 
