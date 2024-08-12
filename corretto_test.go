5ea0c798eff569ca19cbcb6170d08dbdfd9fc478
Luca Zani <lucazani2002@gmail.com>
2024-08-12 21:34:48 +0200

    test: added kitchen sink test

--- corretto_test.go
+++ corretto_test.go
@@ -1,6 +1,8 @@
 package corretto
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -47,4 +49,166 @@ func TestValidationOpts(t *testing.T) {
 			})
 		}
 	})
+}
+
+func TestKitchenSink(t *testing.T) {
+	type user struct {
+		FirstName string
+		LastName  string
+		Status    string
+		Age       int
+		BirthDate string
+		Email     string
+		Children  []user
+		Hobbies   []string
+	}
+
+	uniqueHobbies := func(ctx Context, value reflect.Value) error {
+		// Ensure value is a slice
+		if value.Kind() != reflect.Slice {
+			return fmt.Errorf("expected a slice, got %s", value.Kind())
+		}
+
+		// Get the length of the slice
+		length := value.Len()
+
+		unique := make(map[string]struct{})
+		for i := 0; i < length; i++ {
+			hobby := value.Index(i).String()
+			if _, ok := unique[hobby]; ok {
+				return fmt.Errorf("Hobbies must be unique")
+			}
+			unique[hobby] = struct{}{}
+		}
+		return nil
+	}
+
+	userSchema := Schema{
+		"FirstName": Field("Name").String().MinLength(3).MaxLength(20),
+		"LastName":  Field().String().MinLength(3).MaxLength(20),
+		"Status":    Field().String().OneOf([]string{"active", "inactive"}),
+		"Age":       Field().Number().NonNegative(),
+		"BirthDate": Field().String().NonEmpty(),
+		"Email":     Field().String().Email(),
+		"Hobbies":   Field().Array().Of(Field().String().NonEmpty()).MaxLength(5).Test(uniqueHobbies),
+	}
+
+	hasParentLastName := func(ctx Context, value reflect.Value) error {
+		parent, ok := ctx.(user)
+		if !ok {
+			return fmt.Errorf("expected context to be of type user, got %T", ctx)
+		}
+
+		for i := 0; i < value.Len(); i++ {
+			child := value.Index(i).Interface().(user)
+			if child.LastName != parent.LastName {
+				return fmt.Errorf("Children must have the same last name as the parent")
+			}
+		}
+
+		return nil
+	}
+
+	childSchema := Schema{
+		"FirstName": Field("Name").String().MinLength(3).MaxLength(20),
+		"LastName":  Field().String().MinLength(3).MaxLength(20),
+		"Age":       Field().Number().NonNegative(),
+		"BirthDate": Field().String().NonEmpty(),
+	}
+	userSchema.Concat(Schema{
+		"Children": Field().Array().Of(Field().Schema(childSchema)).Test(hasParentLastName),
+	})
+
+	tests := []struct {
+		name        string
+		user        user
+		expectError bool
+		expectedMsg string
+	}{
+		{
+			name: "valid user",
+			user: user{
+				FirstName: "John",
+				LastName:  "Doe",
+				Status:    "active",
+				Age:       30,
+				BirthDate: "1990-01-01",
+				Email:     "jhon@doe.com",
+				Hobbies:   []string{"reading", "running"},
+				Children: []user{
+					{
+						FirstName: "Jane",
+						LastName:  "Doe",
+						Age:       10,
+						BirthDate: "2010-01-01",
+					},
+				},
+			},
+			expectError: false,
+		},
+		{
+			name: "duplicate hobbies",
+			user: user{
+				FirstName: "John",
+				LastName:  "Doe",
+				Status:    "active",
+				Age:       30,
+				BirthDate: "1990-01-01",
+				Email:     "jhon@doe.com",
+				Hobbies:   []string{"reading", "reading"},
+				Children: []user{
+					{
+						FirstName: "Jane",
+						LastName:  "Doe",
+						Age:       10,
+						BirthDate: "2010-01-01",
+					},
+				},
+			},
+			expectError: true,
+			expectedMsg: "Hobbies must be unique",
+		},
+		{
+			name: "children with different last name",
+			user: user{
+				FirstName: "John",
+				LastName:  "Doe",
+				Status:    "active",
+				Age:       30,
+				BirthDate: "1990-01-01",
+				Email:     "jhon@doe.com",
+				Hobbies:   []string{"reading", "running"},
+				Children: []user{
+					{
+						FirstName: "James",
+						LastName:  "Smith",
+						Age:       12,
+						BirthDate: "2012-01-01",
+					},
+					{
+						FirstName: "Jane",
+						LastName:  "Doe",
+						Age:       10,
+						BirthDate: "2010-01-01",
+					},
+				},
+			},
+			expectError: true,
+			expectedMsg: "Children must have the same last name as the parent",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := userSchema.Parse(tt.user)
+			if tt.expectError && err == nil {
+				t.Errorf("Parse() should have returned an error")
+			}
+
+			if tt.expectError && err != nil && err.Error() != tt.expectedMsg {
+				t.Errorf("Parse() should have returned an error with the expected message")
+				t.Errorf("expected: %s, got: %s", tt.expectedMsg, err.Error())
+			}
+		})
+	}
 }
